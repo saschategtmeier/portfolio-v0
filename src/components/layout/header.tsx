437d18cf4147e9504dc32ae51e22c30a1b05ba80
saschategtmeier <accounts@saschategtmeier.dev>
2022-12-22 11:25:40 +0100

    Add more pronounced backdrop blur on mobile nav

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -5,11 +5,15 @@ import { useBreakpoint } from '../../hooks';
 import { useLayoutStore } from '../../state';
 
 export const Header: FC = () => {
-  const { currentPageSection } = useLayoutStore();
+  const { currentPageSection, isNavbarOpen } = useLayoutStore();
   const currentBreakpoint = useBreakpoint();
 
   return (
-    <div className="fixed top-0 z-50 w-full bg-gradient-to-b from-white/5 to-white/0 pt-4 pb-1 backdrop-blur-[1px] dark:from-black/5 dark:to-black/0">
+    <div
+      className={`fixed top-0 z-50 w-full bg-gradient-to-b from-white/5 to-white/0 pt-4 pb-1 dark:from-black/5 dark:to-black/0 ${
+        isNavbarOpen ? 'backdrop-blur-sm' : 'backdrop-blur-[1px]'
+      }`}
+    >
       <Container>
         <div className="flex justify-between">
           <h1>{currentPageSection?.title ?? ''}</h1>

dee4d34e5b643eac34c1a2f378fa1dfe8cb184d9
saschategtmeier <accounts@saschategtmeier.dev>
2022-12-29 14:15:50 +0100

    Add toast enter and leave animations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,22 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    extend: {
+      animation: {
+        enter: 'enter 200ms ease-out',
+        leave: 'leave 150ms ease-in forwards',
+      },
+      keyframes: {
+        enter: {
+          '0%': { transform: 'scale(0.9)', opacity: 0 },
+          '100%': { transform: 'scale(1)', opacity: 1 },
+        },
+        leave: {
+          '0%': { transform: 'scale(1)', opacity: 1 },
+          '100%': { transform: 'scale(0.9)', opacity: 0 },
+        },
+      },
+    },
     container: {
       screens: {
         sm: '100%',
